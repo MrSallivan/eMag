74da0c0ee9f5a2cb1a57c48b904c450b6e9ba8c8
MrSallivan <nik.sallivan@gmail.com>
2023-07-29 14:04:37 +1000

    navigo

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ const app = document.getElementById("app")
 const header = getHeader()
 const pageContainer = getPageContainer()
 
+
 export async function navigation(page) {
   pageContainer.innerHTML = ""
   switch (page) {
@@ -31,4 +32,23 @@ export async function navigation(page) {
 }
 
 navigation()
+
+const router = new Navigo("/")
+router.on("/", async () => {
+  const pageModuleMain = await import("./pages/main.js")
+  const mainPage = pageModuleMain.getMainPage()
+  pageContainer.append(mainPage)
+})
+router.on("/catalog", async () => {
+  const pageModuleCatalog = await import("./pages/catalog.js")
+  const catalogPage = pageModuleCatalog.getCatalogctPage()
+  pageContainer.append(catalogPage)
+})
+router.on("/basket", async () => {
+  const pageModuleBasket = await import("./pages/basket.js")
+  const basketgPage = pageModuleBasket.getBasketPage()
+  pageContainer.append(basketgPage)
+})
+
+
 app.append(header, pageContainer)
